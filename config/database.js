a8d712258e1acbea2e1fa574b9039f16341d4fb1
Zacaria CHTATAR <zc@42.am>
2016-12-04 14:11:49 +0100

    [FIX] : db connection string

--- config/database.js
+++ config/database.js
@@ -4,7 +4,18 @@ import mongoose from 'mongoose';
 import config from '../config';
 
 const init = () => {
-    mongoose.connect(config.database);
+    
+    // default to a 'localhost' configuration:
+    var connection_string = config.database;
+    // if OPENSHIFT env variables are present, use the available connection info:
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+        connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    mongoose.connect(connection_string);
     mongoose.Promise = global.Promise;
 };
 
