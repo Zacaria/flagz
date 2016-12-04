f7d4a96fa2bfedfc3a7216f38f7c9741f2b94da6
Zacaria CHTATAR <zc@42.am>
2016-12-04 22:43:28 +0100

    [FIX] openshift port

--- src/bin/www.js
+++ src/bin/www.js
@@ -12,7 +12,7 @@ import http from 'http';
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.OPENSHIFT_NODEJS_PORT || '3000');
+const port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 
 /**
