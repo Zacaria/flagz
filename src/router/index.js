3d438108c49da37759631dbe51bbf0ec5a2f2875
Zacaria CHTATAR <zc@42.am>
2016-12-13 22:16:40 +0100

    [UP] : welcomse message

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ const app = express.Router();
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Welcome, doc incoming !'
+        message: 'Welcome guys, doc incoming !'
     });
 });
 
