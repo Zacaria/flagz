a7104679c0219d977e68f6b514df96badec0d982
Zacaria CHTATAR <zc@42.am>
2016-12-25 02:37:06 +0100

    [UP] : deploy dist

--- doc/api_project.js
+++ doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-12-25T01:34:51.523Z",
+    "time": "2016-12-25T01:37:04.099Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
