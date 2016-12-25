0197b87da8e86a2e5102631c5ff6d1fb80289224
Zacaria CHTATAR <zc@42.am>
2016-12-25 02:33:05 +0100

    [UP] : deploy dist

--- doc/api_project.js
+++ doc/api_project.js
@@ -2,13 +2,13 @@ define({
   "title": "FlagZ Documentation",
   "url": "http://flagz-chtatarz.rhcloud.com",
   "name": "flagz",
-  "version": "0.6.2",
+  "version": "0.6.3",
   "description": "flagz POC",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-12-25T01:16:05.071Z",
+    "time": "2016-12-25T01:33:02.831Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
