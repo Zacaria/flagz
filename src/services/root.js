8104e24edc13fd5f8674e5963552b102d8752ab7
Zacaria CHTATAR <zc@42.am>
2016-12-25 23:54:08 +0100

    [UP] : add db link to route root

--- src/services/root.js
+++ src/services/root.js
@@ -1,10 +1,10 @@
-import path from 'path';
 import pjson from '~/package.json';
 
 export const websiteRoot = ({protocol, host}) => ({
     message : 'Welcome guys, doc currently building !',
     version : pjson.version,
-    doc     : 'http://flagz-chtatarz.rhcloud.com/doc',
+    doc     : protocol + '://' + host + '/doc',
+    db      : protocol + '://' + host + '/rockmongo',
     signup  : protocol + '://' + host + '/api/signup',
     signin  : protocol + '://' + host + '/api/signin',
     users   : protocol + '://' + host + '/api/users',
