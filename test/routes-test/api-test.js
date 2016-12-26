5166efca17b4a83a0f5445b0756563886f9166a0
Zacaria CHTATAR <zc@42.am>
2016-12-26 23:45:54 +0100

    [UP] : api tests

--- test/routes-test/api-test.js
+++ test/routes-test/api-test.js
@@ -24,24 +24,24 @@ describe('Api', () => {
             .then(() => done());
     });
 
-    after((done) => { //Before each test we empty the database
+    after((done) => { //After each test we empty the database
         User.remove({})
             .then(() => done());
     });
 
-    it('should not be able to GET the root api without token', (done) => {
+    it('should not GET the root api without token', (done) => {
         chai.request(server)
             .get('/api')
             .end((err, res) => {
                 res.should.have.status(403);
                 res.body.should.be.a('object');
                 res.body.should.have.property('success').eql(false);
-                res.body.should.have.property('message');
+                res.body.should.have.property('message').eql('No token');
                 done();
             });
     });
 
-    it('should not be able to GET the root api with bad token', (done) => {
+    it('should not GET the root api with bad token', (done) => {
         chai.request(server)
             .get('/api?token=0')
             .end((err, res) => {
@@ -53,7 +53,7 @@ describe('Api', () => {
             });
     });
 
-    it('should able to GET the root api with valid token', (done) => {
+    it('should GET the root api with valid token', (done) => {
         userService.authenticate({
                 name: 'admin',
                 password: 'admin'
@@ -71,7 +71,7 @@ describe('Api', () => {
         });
     });
 
-    it('should able to GET the root api with valid token through headers', (done) => {
+    it('should GET the root api with valid token through headers', (done) => {
         userService.authenticate({
                 name: 'admin',
                 password: 'admin'
