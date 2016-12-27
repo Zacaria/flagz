6c42c5063aa7161a6bdf99684196793df6c4a26b
Zacaria CHTATAR <zc@42.am>
2016-12-27 22:49:30 +0100

    [ADD] : routes in contants for test purpose

---
+++ src/constants/routes.js
@@ -0,0 +1,20 @@
+export const ROUTE_ROOT = '/';
+
+export const ROUTE_HEALTH = '/health';
+
+export const ROUTE_INFO = '/info';
+export const ROUTE_INFO_POLL = '/info/poll';
+export const ROUTE_INFO_GEN = '/info/gen';
+
+export const ROUTE_AUTH_SIGNIN = '/api/signin';
+export const ROUTE_AUTH_SINGUP = '/api/signup';
+
+export const ROUTE_USERS = '/api/users';
+export const ROUTE_USERS_ID = '/api/users/:id';
+export const ROUTE_USERS_FRIENDS = '/api/users/friends';
+
+export const ROUTE_MESSAGES = '/api/messages';
+export const ROUTE_MESSAGES_ME = '/api/messages/me';
+export const ROUTE_MESSAGES_RANGE = '/api/messages/@:center&r=:r';
+
+
