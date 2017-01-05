f34fc2e5355f44ba4271ae8709fdcdbbc162b35f
Zacaria CHTATAR <zc@42.am>
2017-01-05 08:08:31 +0100

    [UP] : more auth-tests

--- test/routes-test/auth-test.js
+++ test/routes-test/auth-test.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'test';
 
 import User from '~/src/models/user';
 import * as routePaths from '~/src/constants/routes';
-
+import * as infos from '~/src/constants/infos';
 
 import chai from'chai';
 import chaiHttp from 'chai-http';
@@ -36,7 +36,7 @@ describe('Auth', () => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('success').eql(false);
-                    res.body.should.have.property('info');
+                    res.body.should.have.property('info').eql(PARAMS_ERROR);
                     done();
                 });
         });
@@ -70,49 +70,83 @@ describe('Auth', () => {
 
     describe('/POST signin', () => {
 
-        it('should not authenticate with user not existing', (done) => {
-            chai.request(server)
-                .post(routePaths.ROUTE_AUTH_SIGNIN)
-                .send({
-                    name: 'noone',
-                    password: 'wrong'
-                })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('success').eql(false);
-                    res.body.should.have.property('info');
-                    done();
-                });
+        describe('with bad params', () => {
+            it('should fail on missing name parameter', (done) => {
+                chai.request(server)
+                    .post(routePaths.ROUTE_AUTH_SIGNIN)
+                    .send({
+                        password: 'wrong'
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(false);
+                        res.body.should.have.property('info').eql(infos.PARAMS_ERROR);
+                        done();
+                    });
+            });
+
+            it('should fail on missing password parameter', (done) => {
+                chai.request(server)
+                    .post(routePaths.ROUTE_AUTH_SIGNIN)
+                    .send({
+                        name: 'wrong'
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(false);
+                        res.body.should.have.property('info').eql(infos.PARAMS_ERROR);
+                        done();
+                    });
+            });
         });
 
-        it('should not authenticate with wrong pw', (done) => {
-            chai.request(server)
-                .post(routePaths.ROUTE_AUTH_SIGNIN)
-                .send({
-                    name: 'admin',
-                    password: 'wrong'
-                })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('success').eql(false);
-                    res.body.should.have.property('info');
-                    done();
-                });
-        });
+        describe('with good params', () => {
+            it('should not authenticate with user not existing', (done) => {
+                chai.request(server)
+                    .post(routePaths.ROUTE_AUTH_SIGNIN)
+                    .send({
+                        name: 'noone',
+                        password: 'wrong'
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(false);
+                        res.body.should.have.property('info');
+                        done();
+                    });
+            });
 
-        it('should return a token', (done) => {
-            chai.request(server)
-                .post(routePaths.ROUTE_AUTH_SIGNIN)
-                .send(user)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('success').eql(true);
-                    res.body.should.have.property('token');
-                    done();
-                });
+            it('should not authenticate with wrong pw', (done) => {
+                chai.request(server)
+                    .post(routePaths.ROUTE_AUTH_SIGNIN)
+                    .send({
+                        name: 'admin',
+                        password: 'wrong'
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(false);
+                        res.body.should.have.property('info');
+                        done();
+                    });
+            });
+
+            it('should return a token', (done) => {
+                chai.request(server)
+                    .post(routePaths.ROUTE_AUTH_SIGNIN)
+                    .send(user)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(true);
+                        res.body.should.have.property('token');
+                        done();
+                    });
+            });
         });
     });
 });
